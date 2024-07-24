ba7aa553d81a106d017ed7276bfd19e00aad4a1f
Yuriy <j.a.sharapov@gmail.com>
2024-07-25 03:19:33 +0500

    Задание 2. Создайте Mongoose-схему для коллекции «books»

---
+++ models/book.js
@@ -0,0 +1,36 @@
+const {Schema, model} = require('mongoose')
+
+const bookSchema = new Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        default: ""
+    },
+    authors: {
+        type: String,
+        default: ""
+    },
+    favorite: {
+        type: Boolean,
+        default: false
+    },
+    fileCover: {
+        type: String,
+        default: ""
+    },
+    fileName: {
+        type: String,
+        default: ""
+    },
+    fileBook: {
+        type: String,
+        default: ""
+    }
+})
